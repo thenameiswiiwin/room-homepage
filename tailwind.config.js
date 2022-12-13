b35a5e15a8a871e08d193b0855f7c0a390c1c304
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-12-13 17:29:29 -0600

    design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['League Spartan', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          100: 'hsl(var(--color-primary-100) / <alpha-value>)',
+          200: 'hsl(var(--color-primary-200) / <alpha-value>)',
+          300: 'hsl(var(--color-primary-300) / <alpha-value>)',
+          400: 'hsl(var(--color-primary-400) / <alpha-value>)',
+        },
+      },
+    },
   },
   plugins: [],
 };
